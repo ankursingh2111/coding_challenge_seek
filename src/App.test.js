474d01c9245bf98638699dfe7431ddc5fc397b40
ankur21 <ankursingh2111@gmail.com>
2019-10-30 12:59:38 +1100

    Added unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+import { shallow, mount } from "enzyme";
+
+describe("Testing the Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  describe("Testing the App Componenet", () => {
+    let AppComponent;
+    beforeEach(() => {
+      AppComponent = shallow(<App />);
+    });
+    it("form component is rendered", () => {
+      expect(AppComponent.find("form").length).toEqual(1);
+    });
+
+    it("App component has 2 children", () => {
+      expect(AppComponent.children().length).toEqual(2);
+    });
+    it("form component has 3 div element", () => {
+      expect(AppComponent.find("div").length).toEqual(3);
+    });
+  });
+  describe("Testing the form component", () => {
+    let AppComponent;
+    beforeEach(() => {
+      AppComponent = mount(<App />);
+    });
+    it("form component has a child label", () => {
+      expect(AppComponent.find("label").length).toEqual(1);
+    });
+    it("form component has input element", () => {
+      expect(AppComponent.find("input").length).toEqual(3);
+    });
+  });
 });
